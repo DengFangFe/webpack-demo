663b48f7b67fa22afa888b553b388749c7e7af81
邓芳 <36882215@qq.com>
2017-12-05 14:41:13 +0800

    环境搭建提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-	entry: {
-		app: ["webpack/hot/dev-server", "./app/app.js"],
-	},
-	output: {	
-		path: './output',
-		filename: 'bundle.js'
-	},
-	module: {
-	  loaders: [
-			{ test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader' },
-			{ test: /\.css$/, loader: 'style-loader!css-loader' },
-			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-			{ test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
-	  ]
-	}
+    entry: './js/index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            { test: /\.less$/, loader: ExtractTextPlugin.extract('css!less') },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+            { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css', {
+            allChunks: true
+        })
+    ]
 };
